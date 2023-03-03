e909d01491afe2697a70eaca31dfbf9ea1b743b8
Alireza <77020648+seyedalhae@users.noreply.github.com>
2023-03-04 01:05:18 +0330

    Adding html template

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const nodemailer = require("nodemailer");
+const { html } = require("./template");
 
 var transporter = nodemailer.createTransport({
 	service: "gmail",
 	auth: {
 		user: "seyedalhae@gmail.com",
-		pass: "",
+		pass: "YOUR_PASSWORD",
 	},
 });
 
@@ -12,8 +13,11 @@ var mailOptions = {
 	from: "seyedalhae@gmail.com",
 	to: "royal.lighting.store@gmail.com",
 	subject: "Sending Email using Node.js",
-	text: "That was easy Ha Ha!"
+	// text: "That was easy Ha Ha!",
+	html
 };
+
+console.log(html);
 
 transporter.sendMail(mailOptions, function (error, info) {
 	if (error) {
---
+++ src/template.js
@@ -0,0 +1,4 @@
+const html = `<h1 style="background-color: green; font-family: 'Vazir'; text-align: center;">فروشگاه اینترنتی رویال لایتینگ</h1>
+<p style="background-color: yellow;">از خرید شما سپاسگزاریم!</p>`;
+
+exports.html = html;
