f505885750a646b035e363b973213343e5b7c45f
Alireza <77020648+seyedalhae@users.noreply.github.com>
2023-03-04 00:43:37 +0330

    Complete sending email
    Use nodemailer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
+var transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: "seyedalhae@gmail.com",
+		pass: "",
+	},
+});
 
+var mailOptions = {
+	from: "seyedalhae@gmail.com",
+	to: "royal.lighting.store@gmail.com",
+	subject: "Sending Email using Node.js",
+	text: "That was easy Ha Ha!"
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+	if (error) {
+		console.log("Error: " + error);
+	} else {
+		console.log("Email sent: " + info.response);
+	}
+});
